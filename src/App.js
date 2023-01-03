d32c9f2fc79224d33ea9ef98b10db09f4dc6e7d8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-03 14:59:27 +0900

    Feat : 프로젝트 기초 공사1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,43 @@ import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 
-import RouteTest from "./components/RoutTest";
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("click left")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("click right")}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        <MyButton
+          text={"button"}
+          onClick={() => alert("button click")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"button"}
+          onClick={() => alert("button click")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("button click")}
+          type={"default"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
@@ -20,7 +50,6 @@ function App() {
           <Route path="/diary/:id" element={<Diary />} />
           /* 일기 아이디가 존재하지 않을 이유가 없으므로 이대로 둠 */
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
